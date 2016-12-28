e3dc65c9bf5e8d1121d870acccb743a433723340
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-28 07:28:52 +0100

    Create a program with Wish

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
 import Wish exposing (Model)
-import Html exposing (text)
+import Html.App exposing (program)
 
 model : Model
 model = Wish.init 5
 
-
+main : Program Never
 main =
-    text "Hello, World!"
+    program {
+          init = (Wish.init 5, Cmd.none)
+        , view = Wish.view
+        , update = Wish.update
+        , subscriptions = Wish.subscriptions
+        }
--- src/Wish.elm
+++ src/Wish.elm
@@ -1,14 +1,17 @@
-module Wish exposing (Model, init, view)
+module Wish exposing (Model, init, view, update, subscriptions)
 
 import Html exposing (Html,text)
 
+
 type Message =
     DoNothing
+
 
 type alias Model =
     {
         state: Int
     }
+
 
 init: Int -> Model
 init n =
@@ -21,3 +24,12 @@ view: Model -> Html Message
 view model =
     text (toString model.state)
 
+
+update: Message -> Model -> (Model, Cmd Message)
+update message model =
+    (model, Cmd.none)
+
+
+subscriptions: Model -> Sub Message
+subscriptions model =
+    Sub.batch []
